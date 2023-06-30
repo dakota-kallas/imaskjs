3b5cae9ad0b67aa84a391b4ad3c7aa5aa8651669
Dakota Kallas <kallas.dakota@gmail.com>
2023-06-30 10:52:42 -0500

    Updated file structure

---
+++ esm/imask.js
@@ -0,0 +1,13 @@
+import './controls/input.js';
+import IMask from './core/holder.js';
+import './core/utils.js';
+import './core/action-details.js';
+import './masked/factory.js';
+import './controls/mask-element.js';
+import './controls/html-input-mask-element.js';
+import './controls/html-mask-element.js';
+import './controls/html-contenteditable-mask-element.js';
+
+/* Copyright for portions of project dakota-kallas/imaskjs are held by uNmAnNeR, 2016 as part of project uNmAnNeR/imaskjs. All other copyright for project dakota-kallas/imaskjs are held by Skyward Inc, 2023. */
+
+export { IMask as default };
